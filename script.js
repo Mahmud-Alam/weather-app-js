b938fe3756e48a0693a0236def6c51d1a49f8159
Mahmud-Alam <official.mahmud23@gmail.com>
2025-01-24 21:15:55 +0600

    change weather image based on weather

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const apiKey = `dacdc9838d5af7c90623f198bb0e7c32`;
 const apiURL = `https://api.openweathermap.org/data/2.5/weather?units=metric&q=`;
 const searchBox = document.querySelector(`.search input`);
 const searchBtn = document.querySelector(`.search button`);
+const weatherIcon = document.querySelector(`.weather-icon`);
 
 async function getWeather(city) {
   const response = await fetch(apiURL + city + `&appid=${apiKey}`);
@@ -16,5 +17,19 @@ searchBtn.addEventListener(`click`, () => {
       Math.round(data.main.temp) + "°c";
     document.querySelector(`.humidity`).innerHTML = data.main.humidity + "%";
     document.querySelector(`.wind`).innerHTML = data.wind.speed + " km/h";
+
+    if(data.weather[0].main === `Clear`) {
+      weatherIcon.src = `images/clear.png`;
+    } else if(data.weather[0].main === `Clouds`) {
+      weatherIcon.src = `images/clouds.png`;
+    } else if(data.weather[0].main === `Rain`) {
+      weatherIcon.src = `images/rain.png`;
+    } else if(data.weather[0].main === `Drizzle`) {
+      weatherIcon.src = `images/drizzle.png`;
+    } else if(data.weather[0].main === `Mist`) {  
+      weatherIcon.src = `images/mist.png`;
+    } else if(data.weather[0].main === `Snow`) {
+      weatherIcon.src = `images/snow.png`;
+    }
   });
 });
